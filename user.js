bd140c96a36cc87b95444f3cf51d6c1b7168a366
ipuclavid <87194497+fcyhesit@users.noreply.github.com>
2026-03-16 18:27:53 -0500

    Update USERS object with new user data

--- user.js
+++ user.js
@@ -1,8 +1,4 @@
 const USERS = {
-  "U001": "Ana Martínez",
-  "U002": "Luis Gómez",
-  "U003": "Carlos Pérez",
-  "U004": "María Rodríguez",
-  "U005": "Juan López"
-  // hasta U100
+  "U001": "Juan Perez",
+  "U002": "Maria Gomez"
 };
